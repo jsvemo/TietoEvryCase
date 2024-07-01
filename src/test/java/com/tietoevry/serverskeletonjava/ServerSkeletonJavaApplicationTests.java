9a8023e2094fcd77f5749722bee6bb952cc8351d
yahya <mohamed.yahya.maad@tietoevry.com>
2024-07-01 17:25:15 +0200

    First commit :D

---
+++ src/test/java/com/tietoevry/serverskeletonjava/ServerSkeletonJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tietoevry.serverskeletonjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSkeletonJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
